724480cad5a4c51c451e1668b60d826f1ea8b997
Bruno Totoli <santosbruno101062@gmail.com>
2022-06-06 15:31:03 -0300

    Initial commit

---
+++ src/main/java/com/bruno/myproject/SpringMyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMyprojectApplication.class, args);
+    }
+
+}
